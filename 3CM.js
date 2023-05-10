4170656e3d750be4fb82e9a55d34e1196e33bd9e
Jorge Islas <Jorgeislas97@gmail.com>
2023-05-10 15:06:53 -0400

    Javascript update 3

--- 3CM.js
+++ 3CM.js
@@ -7,10 +7,20 @@ const statusMsg = document.getElementById("status-msg");
 // Create a reset button element
 const resetButton = document.createElement("button");
 
+const moneyGauge = document.getElementById("money-gauge");
+
+const bet1 = document.getElementById("bet-1");
+
+const bet100 = document.getElementById("bet-100");
+
+let wallet = 100;
+
+let selectedbet = null;
+
+let gameover = false;
+
 resetButton.textContent = "Reset Game";
+
 resetButton.style.display = "none";
-
-
-
 
 let winningIndex = Math.floor(Math.random() * 3);
