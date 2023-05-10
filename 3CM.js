c08e5bd0e8332c0c04e3c2ec966e10507e55b3ce
Jorge Islas <Jorgeislas97@gmail.com>
2023-05-10 11:47:34 -0400

    JsCheckPoint2

--- 3CM.js
+++ 3CM.js
@@ -1,11 +1,13 @@
-let cards = [false, false, false];
+const cards = document.querySelectorAll(".card");
 
-cards[math.floor(math.random() *3 )] = true;
-
-function pickCard(index) {
-    if (cards[index]) {
-        alert("You Won!");
-    } else {
-        alert("You Lost!");
+function checkWinner(cardIndex) {
+    if (gameOver) {
+        return;
     }
-}
+
+    if (selectedBet === null) {
+        statusMsg.textContent = "Place a bet first!";
+        return;
+    }
+
+    let winningIndex = Math.floor(Math.random() * 3);
